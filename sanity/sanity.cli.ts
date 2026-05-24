67a6ba86ea23bf2758e0e3dc430a627f363b99eb
Dhiooonn <diondaman51@gmail.com>
2026-05-24 11:18:53 +0700

    fix(frontend): tambahkan dynamic revalidation di homepage untuk atasi cache Vercel

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     dataset: 'production'
   },
   deployment: {
+    appId: 'det7yr4u8hmsswqih7vg1rnr',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
